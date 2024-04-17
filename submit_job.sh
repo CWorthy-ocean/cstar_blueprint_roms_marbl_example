12ef00ac0afbbe2160afa56ef762be7eb567270f
Dafydd Stephenson <dafydd@derecho5.hsn.de.hpc.ucar.edu>
2024-04-16 19:49:59 -0600

    rename fromrestart_run.sh to start_run.sh, update submit_job.sh to include PBS scheduler compatibility

--- submit_job.sh
+++ submit_job.sh
@@ -1,6 +1,4 @@
-#!/bin/bash                                                                                                                                                                                                            
-
-cstar_env # activate C-Star environment                                                                                                                                                                                
+#!/bin/bash
 
 case "$CSTAR_SYSTEM" in
     osx_arm64_gnu)
@@ -12,20 +10,37 @@ case "$CSTAR_SYSTEM" in
          ;;
     ncar_derecho_intel)
         SCHEDULER=PBS
+	PARTITION=develop
         ;;
     *)
         echo "System $CSTAR_SYSTEM not recognised. Configure your ROMS-MARBL environment using C-Star (https://github.com/CWorthy-ocean/C-Star)"
+	exit 1
         ;;
 esac
 
 case "$SCHEDULER" in
     NONE)
-        ./fromrestart_run.sh
+        ./start_run.sh
         ;;
     PBS)
-        echo "todo next"
+        if [ -z "${ACCOUNT_KEY}" ];then
+            echo "ACCOUNT_KEY environment variable empty. Set using export ACCOUNT_KEY=<your_account_key>."
+            exit 1
+        fi
+	qsub -N "roms_marbl_example_2" \
+	     -o "roms_marbl_example.out" \
+	     -A ${ACCOUNT_KEY} \
+	     -l select=1:ncpus=9,walltime=00:10:00 \
+	     -q ${PARTITION} \
+	     -j oe -k eod \
+	     -V \
+	     start_run.sh	
         ;;
     SLURM)
+	if [ -z "${ACCOUNT_KEY}" ];then
+            echo "ACCOUNT_KEY environment variable empty. Set using export ACCOUNT_KEY=<your_account_key>."
+            exit 1
+        fi
         sbatch --job-name="roms_marbl_example" \
                --output="roms_marbl_example.out" \
                --partition=${PARTITION} \
@@ -34,8 +49,8 @@ case "$SCHEDULER" in
                --account=${ACCOUNT_KEY} \
                --export=ALL \
                --mail-type=ALL \
-               -t 00:10:00 \
-               --wrap="./fromrestart_run.sh"
+               -t 00:30:00 \
+               start_run.sh
         ;;
 esac
 
