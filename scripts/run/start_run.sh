35ebe3ef4f177c14aad881cf24f067fcc0cec34c
“Dafydd <dafydd_stephenson@hotmail.com>
2024-06-13 12:36:14 -0600

    update namelist files

--- scripts/run/start_run.sh
+++ scripts/run/start_run.sh
@@ -1,54 +1,93 @@
 #!/bin/bash
 
 
+SCRIPT_DIR=$(dirname "$(realpath "$0")") # location of this .sh file
+CASEROOT=$(realpath "$SCRIPT_DIR/../..")
+ROMS_EXE=${CASEROOT}/source_mods/ROMS/roms
+INPUT_DIR=${CASEROOT}/input_datasets
+OUTPUT_DIR=${CASEROOT}/output
+ROMS_NAMELIST_DIR=${CASEROOT}/namelists/ROMS
+MARBL_NAMELIST_DIR=${CASEROOT}/namelists/MARBL
+
+if [ ! -e ${ROMS_EXE} ];then
+    echo "ROMS executable (${ROMS_EXE}) not found. Ensure your case root directory (${CASEROOT}) is correct \n
+    and that the model has been successfully compiled in ${CASEROOT}/source_mods/ROMS/"
+    exit 1
+fi
+
+if [ -z ${CSTAR_SYSTEM} ];then
+    if [ ! -z ${LMOD_SYSHOST} ];then
+	case ${LMOD_SYSHOST} in
+	    expanse)
+		CSTAR_SYSTEM=sdsc_expanse_intel
+		;;
+	    derecho)
+		CSTAR_SYSTEM=ncar_derecho_intel
+		;;
+	    perlmutter)
+		CSTAR_SYSTEM=nersc_perlmutter_gnu
+		;;
+	esac
+    elif [[ "$(uname)" == "Darwin" ]] && [[ "$(uname -m)" == "arm64" ]]; then
+	CSTAR_SYSTEM=osx_arm64_gnu
+    fi
+fi
+
+
+
 case "$CSTAR_SYSTEM" in
     osx_arm64_gnu)
         ROMS_EXEC_CMD() {
-            mpirun -n 9 ./roms ./roms.in_${PREFIX}
+            mpirun -n 9 ${ROMS_EXE} ${ROMS_NAMELIST_DIR}/roms.in
         }
         ;;
     sdsc_expanse_intel)
         ROMS_EXEC_CMD() {
-            srun --mpi=pmi2 -n 9 ./roms ./roms.in_${PREFIX}
+            srun --mpi=pmi2 -n 9 ${ROMS_EXE} ${ROMS_NAMELIST_DIR}/roms.in
         }
         ;;
     ncar_derecho_intel)
 	cd $PBS_O_WORKDIR
         ROMS_EXEC_CMD() {
-            mpirun -n 9 ./roms ./roms.in_${PREFIX}	    
+            mpirun -n 9 ${ROMS_EXE} ${ROMS_NAMELIST_DIR}/roms.in
         }
         ;;
     *)
         echo "System $CSTAR_SYSTEM not recognised. Configure your ROMS-MARBL environment using C-Star (https://github.com/CWorthy-ocean/C-Star)"
 	exit 1
-        ;;
+	;;
 esac
 
 
 NP_XI=3; # from code/param.opt
 NP_ETA=3;
 
-if grep -q "\!\# define BIOLOGY_BEC2" code/cppdefs.opt;
+if grep -q "\!\# define BIOLOGY_BEC2" ${CASEROOT}/source_mods/ROMS/cppdefs.opt;
 then
-    if grep -q "\!\# define MARBL\b" code/cppdefs.opt;
+    if grep -q "\!\# define MARBL\b" ${CASEROOT}/source_mods/ROMS/cppdefs.opt;
     then
 	PREFIX=NOBGC # No biology
-    elif grep -q "\# define MARBL\b" code/cppdefs.opt;
+    elif grep -q "\# define MARBL\b" ${CASEROOT}/source_mods/ROMS/cppdefs.opt;
     then
 	 PREFIX=MARBL # BGC with MARBL
     fi
-elif grep -q "\# define BIOLOGY_BEC2" code/cppdefs.opt;
+elif grep -q "\# define BIOLOGY_BEC2" ${CASEROOT}/source_mods/ROMS/cppdefs.opt;
 then
     PREFIX=BEC # BGC with BEC
 fi
 
 
 # Split the initial and boundary conditions for use on multiple CPUs (default 8)
-rundir=$(pwd)
-if [ -e roms ];then rm roms;fi
-ln -s code/roms .
+#rundir=$(pwd)
+#if [ -e roms ];then rm roms;fi
+#ln -s code/roms .
 
-cd INPUT/
+cd ${ROMS_NAMELIST_DIR}
+cp roms.in_${PREFIX} roms.in
+perl -pi -e "s|INPUT_DIR|$INPUT_DIR|g"                   roms.in
+perl -pi -e "s|MARBL_NAMELIST_DIR|$MARBL_NAMELIST_DIR|g" roms.in
+
+cd ${INPUT_DIR}/
 mkdir PARTED/
 for X in {\
 roms_bry_2012.nc,roms_bry_bgc_"${PREFIX}".nc,roms_frc.201112.nc,\
@@ -65,9 +104,9 @@ roms_frc.2012??.nc,roms_frc_bgc.nc,roms_grd.nc,\
     mv -v "${X/.nc}".?.nc PARTED/
     fi
 done
-cd ${rundir}
-if [ ! -d RST ];then mkdir RST;fi
-ln -s ${rundir}/INPUT/PARTED/${PREFIX}_rst.20120103120000.?.nc RST/
+cd ${OUTPUT_DIR}
+#if [ ! -d RST ];then mkdir RST;fi
+#ln -s ${rundir}/INPUT/PARTED/${PREFIX}_rst.20120103120000.?.nc RST/
 
 #mpirun -n 9 ./roms ./roms.in_"${PREFIX}"
 ROMS_EXEC_CMD
